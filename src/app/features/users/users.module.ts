b405bb6d4cbba5dadb31f237b51cecb3f5d79f9a
Jimbo9526 <giuseppegabrieledileo95@gmail.com>
2022-03-01 17:37:50 +0100

    first commit

---
+++ src/app/features/users/users.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { UsersDetailsPage } from './users-details.page';
+import { UsersPage } from './users.page';
+import { AuthGuard } from 'src/app/auth/auth.guard';
+import { FormsModule } from '@angular/forms';
+import { DeactivateGuard } from 'src/app/deactivate.guard';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: UsersPage,
+    canActivateChild:[AuthGuard],
+    children: [
+      {
+        path: ':id',
+        component: UsersDetailsPage,
+      },
+    ],
+  }
+
+];
+
+@NgModule({
+  declarations: [UsersPage, UsersDetailsPage],
+  imports: [CommonModule, RouterModule.forChild(routes),FormsModule],
+})
+export class UsersModule {}
---
+++ src/app/navbar.component.ts
@@ -0,0 +1,97 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from './auth/auth.service';
+
+@Component({
+  selector: 'app-navbar',
+  template: `
+    <nav class="navbar navbar-expand navbar-light bg-light">
+      <div class="container-fluid">
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarTogglerDemo01"
+          aria-controls="navbarTogglerDemo01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">Hidden brand</a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a
+                class="nav-link active"
+                aria-current="page"
+                [routerLink]="['/']"
+                routerLinkActive="active"
+                [routerLinkActiveOptions]="{ exact: true }"
+                >Home</a
+              >
+            </li>
+            <li class="nav-item">
+              <a
+                class="nav-link"
+                [routerLink]="['/active-posts']"
+                routerLinkActive="active"
+                >Posts attivi</a
+              >
+            </li>
+            <li class="nav-item" *ngIf="isLoggedIn">
+              <a
+                class="nav-link"
+                [routerLink]="['/inactive-posts']"
+                routerLinkActive="active"
+                >Posts non attivi</a
+              >
+            </li>
+            <li class="nav-item" *ngIf="isLoggedIn">
+              <a
+                class="nav-link"
+                [routerLink]="['/users']"
+                routerLinkActive="active"
+                >Users</a
+              >
+            </li>
+            <li class="nav-item" *ngIf="!isLoggedIn">
+              <a
+                class="nav-link"
+                [routerLink]="['/login']"
+                routerLinkActive="active"
+                >Login</a
+              >
+            </li>
+            <li class="nav-item" *ngIf="!isLoggedIn">
+              <a
+                class="nav-link"
+                [routerLink]="['/signup']"
+                routerLinkActive="active"
+                >Registrati</a
+              >
+            </li>
+          </ul>
+          <p *ngIf="isLoggedIn">Bentornato {{userJson.user.name}}</p>
+          <button class="btn btn-danger mx-3" (click)="onLogout()">logout</button>
+        </div>
+      </div>
+    </nav>
+  `,
+  styles: [],
+})
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  userJson = JSON.parse(<string>localStorage.getItem('user'));
+
+  constructor(private authSrv: AuthService) {}
+
+  onLogout() {
+    this.authSrv.logout();
+  }
+
+  ngOnInit(): void {
+    this.authSrv.isLoggedIn$.subscribe((isLoggedIn) => {
+      this.isLoggedIn = isLoggedIn;
+    })
+  }
+}
